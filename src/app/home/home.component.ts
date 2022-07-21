687e152fb0d7d8938284a4a5d6efd7abb6161202
Giftson Jebaraj <giftson1999@gmail.com>
2022-07-21 19:32:29 +0530

    fix dialogbox something idk men

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Validators } from '@angular/forms';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogComponent } from '../dialog/dialog.component';
+
+
 
 @Component({
   selector: 'app-home',
@@ -21,11 +23,13 @@ export class HomeComponent implements OnInit {
       //   email: this.email,
       //   title: 'Forgot username?'
       // },
+      width: '500px',
+      height: '192px',
+      panelClass: 'custom-dialog-container'
     });
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed')
-
       this.email = result
       console.warn(this.email)
     });
